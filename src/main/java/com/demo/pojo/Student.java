82e22d8468facc3fd59534e99c8104613d4ce3c4
feiniao_glb <feiniao_glb@itluma.com>
2019-08-22 16:35:19 +0800

    张三开发的项目1

---
+++ src/main/java/com/demo/pojo/Student.java
@@ -0,0 +1,35 @@
+package com.demo.pojo;
+
+/**
+ * @author 飞鸟
+ * @date 2019/8/22 - 16:26
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private int age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
